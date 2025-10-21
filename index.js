2d4625bb5b8d6fa31764049446252222f25acf98
Tdyuf <150161991+Tdyuf@users.noreply.github.com>
2025-10-21 05:49:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT; // مهم جدًا
+const PORT = process.env.PORT || 3000; // Railway يعطي PORT تلقائي
 
 app.get('/', (req, res) => {
   res.send('Server is running ✅');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
