f831a5ab3463dce2150c8aaea3d78ac5a094dbf6
Tdyuf <150161991+Tdyuf@users.noreply.github.com>
2025-10-21 12:35:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000; // Railway يعطي PORT تلقائي
-
-app.get('/', (req, res) => {
-  res.send('Server is running ✅');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+npm install discord.js minecraft-server-util
