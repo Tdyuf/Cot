1f3bfb2eb17c29533a07440e022469e39e7e379c
Tdyuf <150161991+Tdyuf@users.noreply.github.com>
2025-10-21 05:45:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT; // مهم جدًا
 
 app.get('/', (req, res) => {
   res.send('Server is running ✅');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
